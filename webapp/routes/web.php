4c6b622104232949a53e1a04fe5cb72e760bb433
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2024-12-27 23:26:42 +0700

    Move code from Mockup to become PHP Blade files

--- webapp/routes/web.php
+++ webapp/routes/web.php
@@ -4,14 +4,34 @@ use App\Http\Controllers\ProductDataFileController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Redirect the index.html route to the index route to avoid confusion
-Route::get('index.html', function () {
-    return redirect()->route('index');
-});
+Route::get('/', fn() => view('index'))->name('index');
+Route::get('/chitietsanpham', fn() => view('chitietsanpham'))->name('chitietsanpham');
+Route::get('/createProductJson', fn() => view('createProductJson'))->name('createProductJson');
+Route::get('/giohang', fn() => view('giohang'))->name('giohang');
+Route::get('/gioithieu', fn() => view('gioithieu'))->name('gioithieu');
+Route::get('/lienhe', fn() => view('lienhe'))->name('lienhe');
+Route::get('/nguoidung', fn() => view('nguoidung'))->name('nguoidung');
+Route::get('/tintuc', fn() => view('tintuc'))->name('tintuc');
+Route::get('/trungtambaohanh', fn() => view('trungtambaohanh'))->name('trungtambaohanh');
+Route::get('/tuyendung', fn() => view('tuyendung'))->name('tuyendung');
+Route::get('/admin', fn() => view('admin'))->name('admin');
 
-Route::get('/', function () {
-    return view('index');
-})->name('index');
+/**
+ * Redirect the *.html route to the index route to avoid confusion
+ * @TODO: This is a temporary solution to avoid confusion between the mockup and webapp. It should be remove in future.
+ */
+Route::get('/{any}.html', function ($any) {
+    // Check if the route is 'index' and redirect to the 'index' route
+    if ($any === 'index') {
+        return redirect()->route('index');
+    }
+
+    // Build the URL without the .html suffix
+    $url = "/$any" . Str::after(request()->getRequestUri(), '.html');
+
+    // Perform redirect 301 to the same route without .html and preserve the query string
+    return redirect($url, 301);
+})->where('any', '.*');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -23,6 +43,11 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+/**
+ * @note
+ * This is a temporary solution to serve the JavaScript file containing the product data.
+ * In a real-world application, you should use a proper API endpoint to serve the data.
+ */
 Route::get('/server-serve-products-data.js', [ProductDataFileController::class, 'serveProductsJs'])->name('data.serveProductsJs');
 
 require __DIR__ . '/auth.php';
